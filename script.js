35e761f015ad43e2b54568865e02680461cdcdbc
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2026-01-23 15:08:56 -0700

    making place function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("in script.js");
+
+populate();
+
+
+
+function populate() {
+    content.replaceChildren();
+    for(i=0; i<DATA.length; i++){
+        place();
+    }
+}
+
+
+function place() {
+    
+}
