e76b37c9441a311778a48853c7258eeb3cfb5f57
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2026-01-23 15:35:32 -0700

    fixed error

--- script.js
+++ script.js
@@ -15,22 +15,22 @@ function populate() {
 function place(g) {
     fig = document.createElement('figure');
         figC = document.createElement('figcaption');
-        figC.innerHTML = DATA[g][1];
-        fig.appendChild(figC);
+            figC.innerHTML = DATA[g][1];
+            fig.appendChild(figC);
 
-                        foto = document.createElement("img");
-                foto.alt = "game cover for " + DATASORTED[g][1];
-                pict = DATASORTED[m][3];
-                if (pict == "") {
-                    console.error("no image url for " + DATASORTED[g][1]);
-                } else {
-                    foto.src = pict;
-                }
-                foto.style.backgroundColor = `hsl(${place * 108}, ${65}%, ${30}%)`;
-                foto.setAttribute("onclick", "goToLink(" + DATASORTED[g][0] + ")");
-                foto.loading = "lazy";
-                foto.decoding = "async";
-                fig.appendChild(foto);
+        foto = document.createElement("img");
+            foto.alt = "game cover for " + DATASORTED[g][1];
+            pict = DATASORTED[g][3];
+            if (pict == "") {
+                console.error("no image url for " + DATASORTED[g][1]);
+            } else {
+                foto.src = pict;
+            }
+            foto.style.backgroundColor = `hsl(${place * 108}, ${65}%, ${30}%)`;
+            foto.setAttribute("onclick", "goToLink(" + DATASORTED[g][0] + ")");
+            foto.loading = "lazy";
+            foto.decoding = "async";
+            fig.appendChild(foto);
     content.appendChild(fig);
 
     console.log("placed");
