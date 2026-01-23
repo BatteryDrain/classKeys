70d656284348f400a7aa215756e291f1210c9588
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2026-01-23 15:31:54 -0700

    added imgs

--- script.js
+++ script.js
@@ -12,11 +12,34 @@ function populate() {
 }
 
 
-function place(i) {
+function place(g) {
     fig = document.createElement('figure');
         figC = document.createElement('figcaption');
-        figC.innerHTML = DATA[i][1];
+        figC.innerHTML = DATA[g][1];
         fig.appendChild(figC);
+
+                        foto = document.createElement("img");
+                foto.alt = "game cover for " + DATASORTED[g][1];
+                pict = DATASORTED[m][3];
+                if (pict == "") {
+                    console.error("no image url for " + DATASORTED[g][1]);
+                } else {
+                    foto.src = pict;
+                }
+                foto.style.backgroundColor = `hsl(${place * 108}, ${65}%, ${30}%)`;
+                foto.setAttribute("onclick", "goToLink(" + DATASORTED[g][0] + ")");
+                foto.loading = "lazy";
+                foto.decoding = "async";
+                fig.appendChild(foto);
     content.appendChild(fig);
+
     console.log("placed");
+}
+
+function goToLink(number){
+    for(index=0; index<DATA.length; index++){
+        if(DATA[index][0] == number){
+            window.open(DATA[index][2], "_self");
+        }
+    }
 }
